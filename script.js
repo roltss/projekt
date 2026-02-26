2804c61270053af4d635f959656b08497474df15
roltss <roland.taklai@gmail.com>
2026-02-26 04:40:41 +0200

    projekt 1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nupp = document.querySelector("#nupp")
 const fakt = document.querySelector("#fakt")
 
 async function getFakt() {
-  const response = await fetch('https://cat-fact.herokuapp.com/facts/random')
+  const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en')
   const data = await response.json()
   fakt.innerText = data.text
 }
